81c331285a03e3ae7167f23424c61a6ee8d12592
James R. Gathje <jamesrgathje@Jamess-MacBook-Pro.local>
2023-01-18 16:37:31 -0500

    feat: create User with encrypted password

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
+from models import db, User
 from secret import SECRET_KEY
 
 app = Flask(__name__)
@@ -9,3 +10,6 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = SECRET_KEY
 
 toolbar = DebugToolbarExtension(app)
+
+with app.app_context():
+	db.init_app(app)
--- models.py
+++ models.py
@@ -3,3 +3,27 @@ from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+
+class User (db.Model):
+	"""SQLAlchemy User Model"""
+
+	__tablename__ = 'users'
+	id = db.Column(db.Integer(), primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	password = db.Column(db.String(), nullable=False)
+
+	@classmethod
+	def register(cls, username, password):
+		hashed = bcrypt.generate_password_hash(password)
+		hashed_utf8 = hashed.decode('utf8')
+		return cls(
+			username=username,
+			password=hashed_utf8
+		)
+	
+	@classmethod
+	def login(cls, username, password):
+		user = User.query.filter_by(username=username).first()
+		if user and bcrypt.check_password_hash(user.password, password):
+			return user
+		return False
