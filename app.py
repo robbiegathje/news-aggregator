4a3676617f8a7efcf2038626df39d99068e9522d
James R. Gathje <jamesrgathje@Jamess-MacBook-Pro.local>
2023-01-18 14:12:33 -0500

    chores: update dependencies & configure app.config

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from secret import SECRET_KEY
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///news'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = SECRET_KEY
+
+toolbar = DebugToolbarExtension(app)
