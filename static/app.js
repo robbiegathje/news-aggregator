17058ea9d1a8f71d565b4cd3a5bf3ff52db239d2
James R. Gathje <jamesrgathje@Jamess-MacBook-Pro.local>
2023-01-25 14:36:46 -0500

    feats: 1) enable app.js to query API /all-stories; 2) enable app.js to query both API endpoints with a search term

--- static/app.js
+++ static/app.js
@@ -1,12 +1,63 @@
 console.log(userLanguageCodes);
 console.log(userCountryCodes);
 
+// Two variables are passed through to app.js from inline HTML scripts when
+// necessary for the page being displayed.
+// The variables are userLanguageCodes and userCountryCodes.
+// Each is an Array[] containing two-character strings''
+// representing either a language or country code for each user selected
+// language and country. See /news/top-stories.html & /news/search.html for
+// the originating scripts that utilize back-end user data
+// directly from the database such that user preferences are always current.
+
+// The parameters for getTopStories() are based on the query string expectations
+// of the internal API. The same is true for other "getStories" functions below.
 async function getTopStories(language, locale, page=1) {
 	let response = await axios.get(
 		'/api/top-stories',
 		{params: {
 			language,
 			locale,
+			page
+		}}
+	);
+	return response
+};
+
+async function getTopStoriesBySearchTerm(
+	search, days, language, locale, page=1
+	) {
+	let response = await axios.get(
+		'/api/top-stories',
+		{params: {
+			search,
+			days,
+			language,
+			locale,
+			page
+		}}
+	);
+	return response
+};
+
+async function getAllStories(language, page=1) {
+	let response = await axios.get(
+		'/api/all-stories',
+		{params: {
+			language,
+			page
+		}}
+	);
+	return response
+};
+
+async function getAllStoriesBySearchTerm(search, days, language, page=1) {
+	let response = await axios.get(
+		'/api/all-stories',
+		{params: {
+			search,
+			days,
+			language,
 			page
 		}}
 	);
