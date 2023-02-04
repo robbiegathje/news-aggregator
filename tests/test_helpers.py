7c51a5072fff6b1b64dd13272b74f9f386c4f089
James R. Gathje <jamesrgathje@Jamess-MacBook-Pro.local>
2023-02-04 10:37:57 -0500

    test: write additional test for API query helper function when called with a number of search days specified

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -10,13 +10,36 @@ class HelperFunctionsTestCase (TestCase):
 		request_args = {
 			API_LANGUAGE_KEY: 'en',
 			API_LOCALE_KEY: 'us',
-			'random': 'crazy'}
+			'random': 'crazy'
+		}
 		query = build_api_query(
 			request_args,
 			API_LANGUAGE_KEY,
-			API_LOCALE_KEY)
+			API_LOCALE_KEY,
+			NUM_OF_DAYS_FOR_SEARCH_KEY
+		)
 
 		self.assertEqual(query[API_TOKEN_KEY], API_TOKEN)
 		self.assertEqual(query[API_LANGUAGE_KEY], 'en')
 		self.assertEqual(query[API_LOCALE_KEY], 'us')
 		self.assertNotIn('random', query)
+		self.assertIsNone(query[NUM_OF_DAYS_FOR_SEARCH_KEY])
+	
+	def test_build_api_query_with_search_days(self):
+		request_args = {
+			API_LANGUAGE_KEY: 'en',
+			API_LOCALE_KEY: 'us',
+			NUM_OF_DAYS_FOR_SEARCH_KEY: '7'
+		}
+		query = build_api_query(
+			request_args,
+			API_LANGUAGE_KEY,
+			API_LOCALE_KEY,
+			NUM_OF_DAYS_FOR_SEARCH_KEY
+		)
+
+		self.assertEqual(query[API_TOKEN_KEY], API_TOKEN)
+		self.assertEqual(query[API_LANGUAGE_KEY], 'en')
+		self.assertEqual(query[API_LOCALE_KEY], 'us')
+		self.assertIn(API_PUBLISHED_AFTER_DATE_KEY, query)
+		self.assertNotIn(NUM_OF_DAYS_FOR_SEARCH_KEY, query)
